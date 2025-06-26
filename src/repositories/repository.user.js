5eb20cc054706839099ccfbea60f79dddfd9848b
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-06-26 14:31:59 -0300

    start

---
+++ src/repositories/repository.user.js
@@ -0,0 +1,111 @@
+import pool from "../database/pool.index.js";
+import { query } from "../database/sqlite.js";
+
+async function Inserir(name, email, phone_number, password, created_at, updated_at) {
+    let sql = `insert into powertech_users(name, email, phone_number, password, created_at, updated_at) values($1, $2, $3, $4, current_timestamp, current_timestamp)
+        returning id_user`;
+    try {
+        const results = await pool.query(`
+                SELECT *
+                FROM powertech_users
+                WHERE email = ${email};
+            `);
+        return results.rows;
+    } catch (e) {
+        const user = await pool.query(sql, [name, email, phone_number, password]);
+        return user.rows[0];
+    }
+}
+
+async function InserirAdmin(name, email, phone_number, password, created_at, updated_at) {
+
+     async function verificaEmailExistente(email) {
+        try {
+            const query = 'SELECT count(*) FROM powertech_admin WHERE email = $1';
+            const result = await pool.query(query, [email]);
+
+            return result.rows[0].count > 0; // Retorna true se o email já existe
+        } catch (error) {
+            console.error('Erro ao verificar email:', error);
+            return false;
+        }
+    }
+
+    const emailJaExiste = await verificaEmailExistente(email);
+    if (emailJaExiste) {
+        console.log('Email já cadastrado.');
+        return [];
+    } else {
+        // console.log('Email disponível para cadastro.'); 
+        let sql = `insert into powertech_admin(name, email, phone_number, password, created_at, updated_at) 
+        values($1, $2, $3, $4, current_timestamp, current_timestamp)
+        returning id_admin`;
+        
+        try {
+            const results = await pool.query(`
+                SELECT *
+                FROM powertech_admin
+                WHERE email = ${email};
+                `);
+                console.log(results);
+                
+                return results.rows;
+            } catch (e) {
+                const admin = await pool.query(sql, [name, email, phone_number, password]);
+                return admin.rows[0];
+            }
+        }  
+}
+
+async function ListarByEmail(email) {
+    let sql = `select * from powertech_users where email = $1`;
+    try {
+        const user = await pool.query(sql, [email]);
+        if (user.length == 0)
+            return [];
+        else
+            return user.rows[0];
+    } catch (err) {
+        console.log(err);
+    }
+}
+async function ListarByEmailAdmin(email) {
+     let sql = `select * from powertech_admin where email = $1`;
+    try {
+        const user = await pool.query(sql, [email]);
+        if (user.length == 0)
+            return [];
+        else
+            return user.rows[0];
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function Profile(id_user) {
+    let sql = `select id_user, name as tecnico, email, 
+    phone_number as celular from powertech_users where id_user = $1`;
+
+    const user = await pool.query(sql, [id_user]);
+    return user.rows[0];
+}
+
+async function ProfileAdmin(id_admin) {
+    let sql = `select id_admin, name as tecnico_admin, email, 
+    phone_number as celular from powertech_admin where id_admin = $1`;
+
+    const admin = await pool.query(sql, [id_admin]);
+    console.log(admin.rows[0]);
+    
+    return admin.rows[0];
+}
+
+async function Listar() {
+
+    let sql = `select id_user, name, email from users order by name`;
+
+    const users = await pool.query(sql, []);
+    return users;
+}
+
+export default { Inserir, InserirAdmin, Listar, ListarByEmail, Profile, ListarByEmailAdmin, ProfileAdmin }
---
+++ src/services/service.user.js
@@ -0,0 +1,84 @@
+import bcrypt from "bcrypt";
+import repoUser from "../repositories/repository.user.js";
+import jwt from "../token.js";
+
+async function Inserir(name, email, phone_number, password) {
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await repoUser.Inserir(name, email, phone_number, hashPassword);
+
+    user.token = jwt.CreateToken(user.id_user);
+
+    return user;
+}
+
+async function InserirAdmin(name, email, phone_number, password) {
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const admin = await repoUser.InserirAdmin(name, email, phone_number, hashPassword);
+
+    admin.token = jwt.CreateToken(admin.id_admin);
+
+    return admin;
+}
+
+async function Login(email, password) {
+
+    const user = await repoUser.ListarByEmail(email);
+
+    if (user.length == 0)
+        return [];
+    else {
+        if (await bcrypt.compare(password, user.password)) {
+            delete user.password;
+
+            user.token = jwt.CreateToken(user.id_user);
+
+            return user;
+        } else
+            return [];
+    }
+}
+
+async function LoginAdmin(email, password) {
+
+    const admin = await repoUser.ListarByEmailAdmin(email);
+
+    if (!admin)
+        return null;
+    else {
+        if (await bcrypt.compare(password, admin.password)) {
+            delete admin.password;
+
+            admin.token = jwt.CreateToken(admin.id_admin);
+
+            return admin;
+        } else
+            return [];
+    }
+}
+
+async function Profile(id_user) {
+
+    const user = await repoUser.Profile(id_user);
+
+    return user;
+}
+
+async function ProfileAdmin(id_admin) {
+
+    const admin = await repoUser.ProfileAdmin(id_admin);
+
+    return admin;
+}
+
+async function Listar() {
+
+    const users = await repoUser.Listar();
+
+    return users;
+}
+
+
+
+export default { Inserir, Login, Profile, InserirAdmin, LoginAdmin, Listar, ProfileAdmin }
