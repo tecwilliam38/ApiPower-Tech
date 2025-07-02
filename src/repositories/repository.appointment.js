dc08aac54f67a1755199e574d3809612a6d986b9
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-07-02 09:25:29 -0300

    appointments

--- src/repositories/repository.appointment.js
+++ src/repositories/repository.appointment.js
@@ -1,4 +1,4 @@
-import { query } from "../database/sqlite.js";
+import pool from "../database/pool.index.js";
 
 // async function Listar(id_user, dt_start, dt_end, id_doctor) {
 
@@ -54,7 +54,7 @@ async function Listar(id_client, dt_start, dt_end, id_tecnico) {
 from powertech_appointments pa
 join powertech_services ps on (ps.id_service = pa.id_service)
 join powertech_tecnicos pt on (pt.id_tecnico = pa.id_tecnico)
-join powertech_clients pc on (pc.id_client = pa.id_client)
+join powertech_client pc on (pc.id_client = pa.id_client)
 join powertech_tecnicos_services pts on (pts.id_tecnico = pa.id_tecnico and 
                         pts.id_service = pa.id_service)
 where pa.id_appointment > 0 `;
@@ -86,59 +86,60 @@ where pa.id_appointment > 0 `;
     return appointments;
 }
 
-async function ListarId(id_appointment) {
+// async function ListarId(id_appointment) {
 
-    let sql = `select a.id_appointment, s.description as service, 
-    d.name as doctor, d.specialty,
-   a.booking_date, a.booking_hour, u.name as user, ds.price, a.id_doctor, 
-   a.id_service, a.id_user
-from appointments a
-join services s on (s.id_service = a.id_service)
-join doctors d on (d.id_doctor = a.id_doctor)
-join users u on (u.id_user = a.id_user)
-join doctors_services ds on (ds.id_doctor = a.id_doctor and 
-                        ds.id_service = a.id_service)
-where a.id_appointment = ? `;
+//     let sql = `select a.id_appointment, s.description as service, 
+//     d.name as doctor, d.specialty,
+//    a.booking_date, a.booking_hour, u.name as user, ds.price, a.id_doctor, 
+//    a.id_service, a.id_user
+// from appointments a
+// join services s on (s.id_service = a.id_service)
+// join doctors d on (d.id_doctor = a.id_doctor)
+// join users u on (u.id_user = a.id_user)
+// join doctors_services ds on (ds.id_doctor = a.id_doctor and 
+//                         ds.id_service = a.id_service)
+// where a.id_appointment = ? `;
 
-    const appointments = await query(sql, [id_appointment]);
+//     const appointments = await query(sql, [id_appointment]);
 
-    return appointments[0];
-}
+//     return appointments[0];
+// }
 
-async function Inserir(id_user,
-    id_doctor, id_service, booking_date, booking_hour) {
+// async function Inserir(id_user,
+//     id_doctor, id_service, booking_date, booking_hour) {
 
-    let sql = `insert into appointments(id_user,
-        id_doctor, id_service, booking_date, booking_hour) 
-        values(?, ?, ?, ?, ?) returning id_appointment`;
+//     let sql = `insert into appointments(id_user,
+//         id_doctor, id_service, booking_date, booking_hour) 
+//         values(?, ?, ?, ?, ?) returning id_appointment`;
 
-    const appointment = await query(sql, [id_user,
-        id_doctor, id_service, booking_date, booking_hour]);
+//     const appointment = await query(sql, [id_user,
+//         id_doctor, id_service, booking_date, booking_hour]);
 
-    return appointment[0];
-}
+//     return appointment[0];
+// }
 
-async function Excluir(id_user, id_appointment) {
+// async function Excluir(id_user, id_appointment) {
 
-    let sql = `delete from appointments 
-    where id_appointment=?`;
+//     let sql = `delete from appointments 
+//     where id_appointment=?`;
 
-    await query(sql, [id_appointment]);
+//     await query(sql, [id_appointment]);
 
-    return { id_appointment };
-}
+//     return { id_appointment };
+// }
 
-async function Editar(id_appointment, id_user,
-    id_doctor, id_service, booking_date, booking_hour) {
+// async function Editar(id_appointment, id_user,
+//     id_doctor, id_service, booking_date, booking_hour) {
 
-    let sql = `update appointments set id_user=?, id_doctor=?, 
-    id_service=?, booking_date=?, booking_hour=? 
-    where id_appointment=?`;
+//     let sql = `update appointments set id_user=?, id_doctor=?, 
+//     id_service=?, booking_date=?, booking_hour=? 
+//     where id_appointment=?`;
 
-    await query(sql, [id_user,
-        id_doctor, id_service, booking_date, booking_hour, id_appointment]);
+//     await query(sql, [id_user,
+//         id_doctor, id_service, booking_date, booking_hour, id_appointment]);
 
-    return { id_appointment };
-}
+//     return { id_appointment };
+// }
 
-export default { Listar, Inserir, Excluir, ListarId, Editar }
+// export default { Listar, Inserir, Excluir, ListarId, Editar }
+export default { Listar }
