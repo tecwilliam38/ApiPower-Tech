956f92191ee7088d0ca99c05c4d5ba18b5cf3514
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-07-07 14:08:06 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import express from "express";
-import cors from "cors";
-import router from "./routes.js";
+import express from 'express';
+import cors from 'cors';
+
+
 
 const app = express();
-
-app.use(express.json());
-
 app.use(cors());
-app.use(router);
+app.use(express.json());
 
 
 app.listen(3001, () => {
