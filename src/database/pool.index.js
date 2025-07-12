a6792ed10043ac1f8a382ed31acbf3803fb2aff4
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-07-12 17:16:36 -0300

    new bd

--- src/database/pool.index.js
+++ src/database/pool.index.js
@@ -2,12 +2,16 @@ import pg from 'pg'
 const { Pool } = pg
 
 const pool = new Pool({
-     user: 'postgres.ffmdyodydnwimwrrjobg',
+     user: 'postgres.xiixmtxqzqhydfrfwoql',
      host: 'aws-0-sa-east-1.pooler.supabase.com',
      database: 'postgres',
-     password: 'XQIv1RBbzXV2QIbq',
+     password: 'Estacio091@@',
      port: 6543,
 });
+// user: 'postgres.ffmdyodydnwimwrrjobg',
+// postgres.xiixmtxqzqhydfrfwoql
+// aws-0-sa-east-1.pooler.supabase.com
+// password: 'XQIv1RBbzXV2QIbq',
 
 
 export default pool;
